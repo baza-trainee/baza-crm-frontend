8d6cb24de6f1f57d86aa88dd9697c392710e1ccf
Kateryna Verveda <katushka211@gmail.com>
2024-07-08 15:35:13 +0200

    update hero section

--- src/pages/landing/Hero.tsx
+++ src/pages/landing/Hero.tsx
@@ -1,9 +1,26 @@
+import Button from '../../components/Button';
 const Hero = () => {
   return (
     <section>
       <h1 className="font-bold text-7xl leading-[108px] text-white">
         Crm Baza Trainee Ukraine
       </h1>
+      <Button
+        label="Увійти"
+        onClick={() => {
+          alert('Увійти');
+        }}
+      />
+      <Button
+        label="Заповнити анкету"
+        onClick={() => {
+          alert('Заповнити анкету');
+        }}
+      />
+      <p className="text-white">
+        Зареєструватися може лише учасник, який подав заявку та пройшов
+        співбесіду з менеджером проєкту через Baza Trainee Ukraine
+      </p>
     </section>
   );
 };
