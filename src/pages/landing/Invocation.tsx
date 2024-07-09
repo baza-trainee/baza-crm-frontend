0cb4bdee148e173c5a630ed5cad055d968664956
Kateryna Verveda <katushka211@gmail.com>
2024-07-09 15:51:30 +0200

    update invocation section

--- src/pages/landing/Invocation.tsx
+++ src/pages/landing/Invocation.tsx
@@ -1,10 +1,21 @@
+import Button from '../../components/Button';
+
 const Invocation = () => {
   return (
-    <section>
-      <p>
-        Заповни анкету на Baza Trainee Ukraine і чекай на запрошення до CRM
-        системи, де ти можеш обрати свій перший проєкт
+    <section className="px-[120px] pt-[50px] pb-[150px] flex flex-col items-center">
+      <p className="min-w-[1021px] h-auto font-lato font-bold leading-[60px] text-[40px] text-white text-center mb-[100px]">
+        Заповни анкету на Baza Trainee Ukraine
+        <br />
+        і чекай на запрошення до CRM системи, де ти можеш <br />
+        обрати свій перший проєкт
       </p>
+      <Button
+        label="Заповнити анкету"
+        onClick={() => {
+          alert('Заповнити анкету');
+        }}
+        additionalClass="bg-primary-blue hover:bg-transparent hover:border-4 hover:border-primary-blue"
+      />
     </section>
   );
 };
