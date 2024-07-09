d391f52a51c566cbaf0dd266f7461f75729c0f9b
Kateryna Verveda <katushka211@gmail.com>
2024-07-09 15:03:26 +0200

    update About section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,8 +6,11 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ text }) => {
   return (
-    <div className="">
-      <p> {text}</p>
+    <div className="p-[50px] w-[539px] h-[264px] rounded border border-solid border-white">
+      <p className="text-xl font-open-sans leading-[30px] font-normal tracking-[0.4px] text-white">
+        {' '}
+        {text}
+      </p>
     </div>
   );
 };
