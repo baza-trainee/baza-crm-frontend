a5f47bb7269e1c2a723223f106fa64996b16025d
hettfield git <olekpl90@gmail.com>
2024-07-15 21:34:04 +0200

    fix

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,35 +1,7 @@
-import React, { useState } from 'react';
+
 import logo from '../../src/assets/common/logo.svg';
 
 const Register = () => {
-
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    confirmPassword: '',
-    agree: false
-  });
-
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFormData({
-      ...formData,
-      [name]: type === 'checkbox' ? checked : value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (formData.password !== formData.confirmPassword) {
-      alert("Паролі не співпадають!");
-      return;
-    }
-    if (!formData.agree) {
-      alert("Ви повинні погодитися з Правилами користування та Політикою конфіденційності!");
-      return;
-    }
-    console.log('Form submitted', formData);
-  };
 
   return (
     <div className='w-full bg-[#071933] pt-[50px] pb-[198px]'>
@@ -39,14 +11,13 @@ const Register = () => {
       <div className='w-[891px] text-center pt-[50px] mx-auto'>
         <h1 className='font-Lato font-sans text-[40px] text-white'>Реєстрація учасника в CRM системі на Baza Trainee Ukraine</h1>
       </div>
-        <form onSubmit={handleSubmit} className='w-[538px] mt-[50px] mx-auto'>
+        <form className='w-[538px] mt-[50px] mx-auto'>
           <div className='flex flex-col'>
             <label className='font-Open Sans font-sans text-[20px] font-normal leading-[1.5] text-white mb-[2.5px]'>Логін (Email)</label>
             <input
               type='email'
               name='email'
-              value={formData.email}
-              onChange={handleChange}
+  
               required
               className='font-Lato font-sans font-normal leading-relaxed text-[16px] bg-[#d2e4ff] rounded-[10px] p-[16px] h-[40px] mb-[23.5px]'
             /> 
@@ -56,8 +27,7 @@ const Register = () => {
             <input
               type='email'
               name='email'
-              value={formData.password}
-              onChange={handleChange}
+              
               required
               className='font-Lato font-sans font-normal leading-relaxed text-[16px] bg-[#d2e4ff] rounded-[10px] p-[16px] h-[40px]  mb-[23.5px]'
             /> 
@@ -67,8 +37,7 @@ const Register = () => {
             <input
               type='email'
               name='email'
-              value={formData.confirmPassword}
-              onChange={handleChange}
+             
               required
               className='font-Lato font-sans font-normal text-[16px] bg-[#d2e4ff] rounded-[10px] p-[16px] h-[40px]  mb-[49px]'
             /> 
@@ -77,8 +46,7 @@ const Register = () => {
             <input
               type='checkbox'
               name='agree'
-              checked={formData.agree}
-              onChange={handleChange}
+              
               required
               className='w-[20px] h-[20px] mt-[4px]'
             />
