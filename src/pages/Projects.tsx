77dd0820801b96d376582a6fd5c9c7cec815975e
jackal7819 <jackal3166@gmail.com>
2024-07-16 00:16:48 +0300

    Add Projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,9 +1,30 @@
+import Project from '../components/Projects/Project';
 import ProjectsHeader from '../components/Projects/ProjectsHeader';
+import ProjectsTabs from '../components/Projects/ProjectsTabs';
+import { projects } from '../data';
 
 const Projects = () => {
   return (
-    <section className="flex flex-col gap-5 px-8 py-5">
+    <section className="flex flex-col w-full gap-5 px-8 py-5 bg-light-blue-bg">
       <ProjectsHeader />
+      <ProjectsTabs />
+      {projects.length === 0 && (
+        <div className="flex flex-col max-w-md gap-3 px-4 py-12 text-xl">
+          <p>
+            На даний момент всі проєкти знаходяться в стадії розробки або
+            завершені.
+          </p>
+          <p>Зараз ми активно шукаємо нові проєкти.</p>
+          <p>Слідкуйте за повідомленнями на нашому Discord каналі.</p>
+        </div>
+      )}
+      {projects.length > 0 && (
+        <div className="grid grid-cols-3 gap-5 lg:grid-cols-4 xl:grid-cols-5">
+          {projects.map((project) => (
+            <Project key={project.id} {...project} />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
