9c8c1a91f5ed4963403c73d7c9bfb62f7a4e4152
Kateryna Verveda <katushka211@gmail.com>
2024-07-22 08:29:58 +0200

    add slider

--- src/components/Landing/Gallery.tsx
+++ src/components/Landing/Gallery.tsx
@@ -1,7 +1,42 @@
-const Gallery = () => {
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import '../Landing/gallery.css';
+
+import slide1 from '../../assets/images/slide-1.webp';
+import slide2 from '../../assets/images/slide-2.webp';
+import slide3 from '../../assets/images/slide-3.webp';
+
+const Gallery: React.FC = () => {
   return (
-    <section>
-      <h1>Gallery</h1>
+    <section className="pt-[50px] pb-[100px]">
+      <Swiper
+        spaceBetween={70}
+        navigation={true}
+        loop={true}
+        slidesPerView={'auto'}
+        centeredSlides={true}
+        modules={[Navigation]}
+        className="landingSwiper"
+      >
+        <SwiperSlide>
+          {' '}
+          <img src={slide1} alt="Slide 1" />
+        </SwiperSlide>{' '}
+        <SwiperSlide>
+          {' '}
+          <img src={slide2} alt="Slide 2" />
+        </SwiperSlide>{' '}
+        <SwiperSlide>
+          {' '}
+          <img src={slide3} alt="Slide 3" />
+        </SwiperSlide>{' '}
+        <SwiperSlide>
+          {' '}
+          <img src={slide1} alt="Slide 1" />
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };
