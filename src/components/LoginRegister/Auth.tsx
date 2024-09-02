8ead6982a3e33c36e7f48b9e118f3c15ee3a79c9
hettfield git <olekpl90@gmail.com>
2024-09-02 11:23:16 +0200

    fix: fix login page

--- src/components/LoginRegister/Auth.tsx
+++ src/components/LoginRegister/Auth.tsx
@@ -1,18 +1,13 @@
-// import axios from 'axios';
-
-// export const registerUser = async (data: { email: string; password: string }) => {
-//   const response = await axios.post('http://185.161.208.63:5000/api/v1/auth/register', data);
-//   return response.data;
-// };
-
 import axios from 'axios';
 
 type RegisterResponse = {
   message: string;
+  stack: string;
   token?: string;
   user?: {
-    id: string;
+    id: number;
     email: string;
+    isAdmin: boolean;
   };
 };
 
