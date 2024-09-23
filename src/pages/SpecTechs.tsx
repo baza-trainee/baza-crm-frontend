814db666430b933a1019c06bd438d2dff854ff12
hettfield git <olekpl90@gmail.com>
2024-09-23 10:34:47 +0200

    add base lists

--- src/pages/SpecTechs.tsx
+++ src/pages/SpecTechs.tsx
@@ -2,26 +2,29 @@ import Button from '../components/Button';
 
 const SpecTechs = () => {
   return (
-    <div className="flex font-lato font-bold text-[20px] leading-[30px] px-[30px] py-[20px]">
-      <div className="w-[412px]">
-        <h1>Спеціалізації</h1>
-        <div className="text-[16px] font-semibold">
+    <div className="flex gap-[165px] font-lato font-bold text-[20px] leading-[30px] px-[30px] py-[40px] bg-light-blue-bg">
+      <div>
+        <h1 className="px-[20px] mb-[10px]">Спеціалізації</h1>
+        <div className="text-[16px] font-semibold p-[20px] bg-white border border-card-border rounded-lg">
           <ul>
-            <li>PM</li>
+            <li className="w-[372px]">PM</li>
             <li>Design</li>
             <li>Front-end</li>
             <li>Back-end</li>
             <li>QA manual</li>
             <li>Full Stack</li>
           </ul>
-          <Button label="Додати спеціалізацію" className="w-[268px]" />
+          <div className="flex justify-center">
+            <Button label="Додати спеціалізацію" className="w-[268px]" />
+          </div>
         </div>
+        <div></div>
       </div>
-      <div className="w-[412px]">
-        <h1>Технології</h1>
-        <div>
+      <div>
+        <h1 className="px-[20px] mb-[10px]">Технології</h1>
+        <div className="text-[16px] font-semibold p-[20px] bg-white border border-card-border rounded-lg">
           <ul>
-            <li>Figma</li>
+            <li className="w-[372px]">Figma</li>
             <li>UX/UI</li>
             <li>Canva</li>
             <li>Adobe Illustrator</li>
@@ -32,8 +35,10 @@ const SpecTechs = () => {
             <li>Swagger</li>
             <li>Postman</li>
           </ul>
+          <div className="flex justify-center">
+            <Button label="Додати технологію" className="w-[220px]" />
+          </div>
         </div>
-        <Button label="Додати технологію" className="w-[220px]" />
       </div>
     </div>
   );
