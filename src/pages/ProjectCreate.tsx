d16e356593f684064bae917e05016a4008c413bf
jackal7819 <jackal3166@gmail.com>
2024-10-03 23:31:01 +0300

    Add dates to ProjectCreate page

--- src/pages/ProjectCreate.tsx
+++ src/pages/ProjectCreate.tsx
@@ -2,18 +2,17 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 import DatePicker from 'react-datepicker';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import { FaCalendarDay } from 'react-icons/fa6';
-import { FaLinkedin, FaSquareFacebook, FaTelegram } from 'react-icons/fa6';
+import calendar from '../assets/common/calendar.svg';
 import { Tooltip } from 'react-tooltip';
 import { toast } from 'react-toastify';
 import { useMutation } from '@tanstack/react-query';
 import { useSelector } from 'react-redux';
 
-import ProjectDropdown from '../components/Projects/ProjectDropdown';
 import ProjectFormat from '../components/Projects/ProjectFormat';
 import { CreateProjectRequest, RootState } from '../types';
 import { createProject } from '../utils/projectApi';
 import { specializations } from '../utils/specializations';
+// import Calendar from 'react-datepicker/dist/calendar'
 
 // import { useState } from 'react';
 
@@ -56,11 +55,11 @@ const ProjectCreate: React.FC = () => {
       {/* TITLE */}
       <div className="h-[60px] flex justify-between items-center font-bold text-text-black bg-white rounded-xl border-card-border border px-8 w-[845px]">
         <input
-          className="duration-500 outline-none w-96"
+          className="duration-500 border-b-2 outline-none w-96 focus:border-b-2 focus:border-b-primary-blue"
           placeholder="Ввести назву проєкту"
           type="text"
           {...register('name', {
-            required: "Назва проєкту обов'язкова для заповнення",
+            required: "Назва проєкту обов'язкова",
             minLength: {
               value: 3,
               message: 'Назва повинна містити щонайменше 3 символи',
@@ -76,42 +75,118 @@ const ProjectCreate: React.FC = () => {
           <span className="text-rose-500">{errors.name.message}</span>
         )}
       </div>
-      {/* DESCRIPTION */}
       <h3 className="mb-3 ml-8 text-xl font-bold">Опис проєкту</h3>
       <div className="flex flex-wrap gap-5 mb-10">
-        <div className="w-[845px] bg-white rounded-[10px] px-8 py-5 border-card-border border flex flex-col justify-between">
-          <p>Ввести опис проєкту</p>
-          {/* <p className="flex justify-between gap-5 font-bold max-w-[440px]">
-            Дата старту формування команди{' '}
-            <span className="ml-14">08/17/2023</span>
-          </p> */}
-          <div>
-            <label htmlFor="dateTeam">Дата формування команди</label>
+        <div className="w-[845px] bg-white rounded-[10px] px-8 py-5 border-card-border border flex flex-col justify-between gap-5">
+          {/* DESCRIPTION */}
+          <input
+            className="duration-500 border-b-2 outline-none w-96 focus:border-b-2 focus:border-b-primary-blue"
+            placeholder="Ввести опис проєкту"
+            type="text"
+            {...register('description', {
+              required: "Опис проєкту обов'язкова",
+              minLength: {
+                value: 3,
+                message: 'Опис проєкту повинен містити щонайменше 3 символи',
+              },
+            })}
+          />
+          <div className="h-5 -mt-5 -mb-3">
+            {errors.description && (
+              <span className="text-rose-500">
+                {errors.description.message}
+              </span>
+            )}
+          </div>
+          {/* DATE TEAM */}
+          <div className="relative flex items-center gap-5 w-[650px]">
+            <label
+              htmlFor="dateTeam"
+              className="font-bold font-lato text-tertiary-text w-[290px]"
+            >
+              Дата старту формування команди
+            </label>
             <Controller
               control={control}
               name="dateTeam"
-              rules={{ required: "Дата формування команди є обов'язковою" }}
+              rules={{
+                required: "Дата старту формування команди є обов'язковою",
+              }}
               render={({ field }) => (
                 <DatePicker
                   selected={field.value ? new Date(field.value) : null}
-                  showIcon
                   onChange={(date) => field.onChange(date)}
                   dateFormat="MM/dd/yyyy"
-                  className="flex flex-row-reverse items-center justify-between w-full ml-5 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                  placeholderText="08/17/2023"
+                  className="w-[320px] h-14 flex items-center rounded border-2 border-card-border px-5 duration-500 outline-none  focus:border-primary-blue focus:border-2"
                 />
               )}
             />
-            <FaCalendarDay className="text-primary-blue" />
-            {errors.dateTeam && <p>{errors.dateTeam.message}</p>}
+            <img
+              src={calendar}
+              alt="calendar"
+              className="absolute top-1 right-7"
+            />
           </div>
-          <p className="flex justify-between gap-5 font-bold max-w-[440px]">
-            Дата старту розробки <span className="ml-14">08/17/2023</span>
-          </p>
+          <div className="h-5 -mt-5 -mb-3">
+            {errors.dateTeam && (
+              <span className="text-rose-500">{errors.dateTeam.message}</span>
+            )}
+          </div>
+          {/* DATE START */}
+          <div className="relative flex items-center gap-5 w-[650px]">
+            <label
+              htmlFor="dateStart"
+              className="font-bold font-lato text-tertiary-text w-[290px]"
+            >
+              Дата старту проєкту
+            </label>
+            <Controller
+              control={control}
+              name="dateStart"
+              rules={{ required: "Дата старту проєкту є обов'язковою" }}
+              render={({ field }) => (
+                <DatePicker
+                  selected={field.value ? new Date(field.value) : null}
+                  onChange={(date) => field.onChange(date)}
+                  dateFormat="MM/dd/yyyy"
+                  placeholderText="08/17/2023"
+                  className="w-[320px] h-14 flex items-center rounded border-2 border-card-border px-5 duration-500 outline-none  focus:border-primary-blue focus:border-2"
+                />
+              )}
+            />
+            <img
+              src={calendar}
+              alt="calendar"
+              className="absolute top-1 right-7"
+            />
+          </div>
+          <div className="h-5 -mt-5 -mb-3">
+            {errors.dateStart && (
+              <span className="text-rose-500">{errors.dateStart.message}</span>
+            )}
+          </div>
         </div>
-        <div className="w-[412px] flex flex-col justify-between gap-5">
-          <div className="bg-white rounded-[10px] px-8 py-2 border-card-border border justify-between flex items-end">
-            <p>Бали за участь</p>
-            <p className="font-semibold">Вказати кількість балів</p>
+        <div className="w-[412px] flex flex-col gap-5">
+          {/* PROJECT POINTS */}
+          <div className="bg-white rounded-[10px] px-8 py-2 border-card-border border justify-between flex items-center">
+            <p className="font-bold font-lato">Бали за участь</p>
+            <input
+              className="duration-500 border-b-2 outline-none w-52 focus:border-b-2 focus:border-b-primary-blue"
+              placeholder="Вказати кількість балів"
+              type="number"
+              min={0}
+              {...register('projectPoints', {
+                required: "Кількість балів обов'язкова і бути числом",
+              })}
+            />
+          </div>
+          <div className="h-5 px-8 -mt-5 -mb-3">
+            {errors.projectPoints && (
+              <span className="text-rose-500">
+                {errors.projectPoints.message}
+              </span>
+            )}
           </div>
           <div className="bg-white rounded-[10px] px-8 py-2 border-card-border border justify-between flex items-end">
             <p>Формат участі</p>
@@ -129,49 +204,16 @@ const ProjectCreate: React.FC = () => {
               <ProjectFormat projectType="light" />
             </Tooltip>
           </div>
-          <ProjectDropdown />
-        </div>
-        <div className="w-[412px] flex flex-col justify-between bg-white rounded-[10px] px-8 py-5 border-card-border border">
-          <div className="flex items-center gap-3">
-            <FaLinkedin color="#1e70eb" size={32} />{' '}
-            <a
-              href="https://www.linkedin.com"
-              className="relative inline-block duration-500 group"
-            >
-              <span className="group-hover:after:w-full after:w-0 after:block after:h-[1px] after:bg-primary-blue after:absolute after:left-0 after:bottom-0 after:transition-all after:duration-500 hover:text-primary-blue">
-                https://www.linkedin.com
-              </span>
-            </a>
-          </div>
-          <div className="flex items-center gap-3">
-            <FaSquareFacebook color="#1e70eb" size={32} />{' '}
-            <a
-              href="https://www.facebook.com"
-              className="relative inline-block duration-500 group"
-            >
-              <span className="group-hover:after:w-full after:w-0 after:block after:h-[1px] after:bg-primary-blue after:absolute after:left-0 after:bottom-0 after:transition-all after:duration-500 hover:text-primary-blue">
-                https://www.facebook.com
-              </span>
-            </a>
-          </div>
-          <div className="flex items-center gap-3">
-            <FaTelegram color="#1e70eb" size={32} />{' '}
-            <a
-              href="https://www.telegram.com"
-              className="relative inline-block duration-500 group"
-            >
-              <span className="group-hover:after:w-full after:w-0 after:block after:h-[1px] after:bg-primary-blue after:absolute after:left-0 after:bottom-0 after:transition-all after:duration-500 hover:text-primary-blue">
-                https://www.telegram.com
-              </span>
-            </a>
-          </div>
         </div>
       </div>
       {/* TEAM */}
       <h3 className="mb-3 ml-8 text-xl font-bold">Склад команди</h3>
       <div className="flex flex-wrap gap-5">
         {specializations.map((specialization) => (
-          <div className="w-[268px] bg-white rounded-[10px] px-8 py-5 border-card-border border h-[282px] flex flex-col justify-start gap-3">
+          <div
+            className="w-[268px] bg-white rounded-[10px] px-8 py-5 border-card-border border h-[282px] flex flex-col justify-start gap-3"
+            key={specialization}
+          >
             <div className="flex items-center justify-between">
               <div className="px-8 py-2 text-white rounded-r-[10px] -ml-8 self-start bg-primary-blue">
                 {specialization}
