dd5aea9e5d20fd0bb28884fbc19e8633c3ad2de2
Yevhenii <jenmikitenko@gmail.com>
2025-01-15 20:13:47 +0200

    fix: fix status error

--- src/pages/ConnectToDiscordProject.tsx
+++ src/pages/ConnectToDiscordProject.tsx
@@ -7,47 +7,30 @@ import { connectDiscordProject } from '../utils/connectBotApi';
 import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
 import { changeStatus } from '../utils/projectStatusApi';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const ConnectToDiscordProject = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const queryClient = useQueryClient();
-
-  const mutationChangeStatus = useMutation({
-    mutationFn: changeStatus,
-    onSuccess: () => {
-      toast.success('Статус успішно змінено');
-      queryClient.invalidateQueries({ queryKey: ['projectWithUsers'] });
-    },
-    onError: () => {
-      toast.error('Не вдалося змінити статус');
-    },
-  });
-  const handleChangeStatus = () => {
-    const params = new URLSearchParams(location.search);
-    const projectId = Number(params.get('state'));
-    const status: string = 'working';
-    const user: Auth = JSON.parse(localStorage.getItem('user')!);
-    if (user && projectId) {
-      mutationChangeStatus.mutate({ status, token: user.token, projectId });
-    }
-  };
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     const projectId = params.get('state');
     const guildId = params.get('guild_id');
     const user: Auth = JSON.parse(localStorage.getItem('user')!);
-
     dispatch(loginUser(user));
 
     const connect = async () => {
       try {
         await connectDiscordProject(user!.token, guildId!, projectId!);
-        handleChangeStatus();
+        await changeStatus({
+          status: 'working',
+          token: user.token!,
+          projectId: Number(projectId),
+        });
+        //handleChangeStatus();
         toast.success('Успішно синхронізовано');
+        toast.success('Статус успішно змінено');
       } catch (err) {
         toast.error('Помилка в синхронізації');
       } finally {
